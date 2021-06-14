59fcd4599fa0681b9f5e0275bbbc158abe59ba70
Thundiii <silasontrup@gmail.com>
2021-06-14 03:30:51 +0200

    ConsoleDiscord 1.0

---
+++ MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace PlayStationDiscord
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Details = new System.Windows.Forms.TextBox();
+            this.playstationSwitch = new System.Windows.Forms.ComboBox();
+            this.tb_State = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Details
+            // 
+            this.tb_Details.Location = new System.Drawing.Point(59, 71);
+            this.tb_Details.Name = "tb_Details";
+            this.tb_Details.PlaceholderText = "Game";
+            this.tb_Details.Size = new System.Drawing.Size(91, 23);
+            this.tb_Details.TabIndex = 2;
+            // 
+            // playstationSwitch
+            // 
+            this.playstationSwitch.FormattingEnabled = true;
+            this.playstationSwitch.Items.AddRange(new object[] {
+            "PlayStation 1",
+            "PlayStation 2",
+            "Wii"});
+            this.playstationSwitch.Location = new System.Drawing.Point(59, 42);
+            this.playstationSwitch.Name = "playstationSwitch";
+            this.playstationSwitch.Size = new System.Drawing.Size(192, 23);
+            this.playstationSwitch.TabIndex = 3;
+            this.playstationSwitch.Text = "Choose Console";
+            this.playstationSwitch.SelectedIndexChanged += new System.EventHandler(this.playstationSwitch_SelectedIndexChanged);
+            // 
+            // tb_State
+            // 
+            this.tb_State.Location = new System.Drawing.Point(151, 71);
+            this.tb_State.Name = "tb_State";
+            this.tb_State.PlaceholderText = "State";
+            this.tb_State.Size = new System.Drawing.Size(100, 23);
+            this.tb_State.TabIndex = 4;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(114, 116);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 7;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 151);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.tb_State);
+            this.Controls.Add(this.playstationSwitch);
+            this.Controls.Add(this.tb_Details);
+            this.Name = "MainForm";
+            this.Text = "PlayStation for Discord";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Details;
+        private System.Windows.Forms.ComboBox playstationSwitch;
+        private System.Windows.Forms.TextBox tb_State;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace PlayStationDiscord
+{
+    public partial class MainForm : Form
+    {
+        public enum Console
+        {
+            ConsoleDiscord,
+            Ps1,
+            Ps2,
+            Wii,
+            Switch
+
+        }
+
+        public static String idleKey = "853801529739575297";
+        public static String WiiKey = "853800132180443136";
+        public static String ps2Key = "853764346027311114";
+
+        public DiscordRpcClient client;
+        public Console con;
+        public MainForm()
+        {
+            InitializeComponent();
+            con = Console.ConsoleDiscord;
+            client = new DiscordRpcClient(idleKey);
+            client.Initialize();
+            setIdle();
+            
+        }
+
+        private void setIdle()
+        {
+            client.SetPresence(new DiscordRPC.RichPresence()
+            {
+                Details = "",
+                State = "Idle...",
+                Timestamps = Timestamps.Now,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "idleicon",
+                    
+                    LargeImageText = getConsole(),
+                }
+
+            });
+        }
+
+        private String getConsole()
+        {
+            switch(con.ToString())
+            {
+                case "Ps2": return "PlayStation 2";
+                case "Wii": return "Wii";
+            }
+            return "ConsoleDiscord";
+        }
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            client.SetPresence(new DiscordRPC.RichPresence()
+            {
+                Details = tb_Details.Text,
+                State = tb_State.Text,
+                Timestamps = Timestamps.Now,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "crash2_logo",
+                    //LargeImageText = playStationVersion
+                }
+
+            }); 
+
+        }
+
+        private void changeConsole(Console c)
+        {
+            switch(c)
+            {
+                case Console.Wii: client.Deinitialize(); con = Console.Wii; client = new DiscordRpcClient(WiiKey); client.Initialize(); setIdle(); break;
+                case Console.Ps2: client.Deinitialize(); con = Console.Ps2; client = new DiscordRpcClient(ps2Key); client.Initialize(); setIdle(); break;
+
+            }
+        }
+
+        private void playstationSwitch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           switch(playstationSwitch.Text)
+            {
+                case "Wii": changeConsole(Console.Wii); break;
+                case "PlayStation 2": changeConsole(Console.Ps2); break;
+            }
+
+        }
+    }
+}
